7d5d379e136678cb9fea5ac83e66b6ac2b081d00
TugceUsta <tugce.gulsan@gmail.com>
2023-12-07 22:40:03 +0300

    Project1 Beck End initialized with S B Initializer

---
+++ src/main/java/com/pinsoft/project1/be/Project1BEApplication.java
@@ -0,0 +1,13 @@
+package com.pinsoft.project1.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1BEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1BEApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pinsoft/project1/be/Project1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinsoft.project1.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
